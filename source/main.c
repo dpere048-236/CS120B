84b8fa4346fd1efe382ddfc8b0464c11e755bbd2
David Perez <dpere048@wch136-12.cs.ucr.edu>
2019-10-08 15:53:18 -0700

    Files modified

--- source/main.c
+++ source/main.c
@@ -14,9 +14,14 @@
 
 int main(void) {
     /* Insert DDR and PORT initializations */
-
+	DDRA = 0x00; PORTA = 0xFF; //port A 8 pins = inputs
+	DDRB = 0xFF; PORTB = 0x00; //portB 8 pins = outputs
+				   // PortB output to 0x00
+	unsigned char temp_input = 0x00;
     /* Insert your solution below */
     while (1) {
+		temp_input = PINA; //portB 8 pins with values on port A 8 pins
+		PORTB = temp_input;
 
     }
     return 1;
